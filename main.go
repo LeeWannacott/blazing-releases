788ba3bf1d816e2b1f81dc10f17c3b3079952018
Lee Wannacott <wannacottL@gmail.com>
2022-01-22 15:11:18 +1300

    regex wip.

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 )
 
 func check(e error) {
@@ -15,5 +17,22 @@ func main() {
 	fmt.Println("quick release notes.")
 	data, err := os.ReadFile("./docs/CHANGELOG.md")
 	check(err)
-	fmt.Print(string(data))
+	// fmt.Println(1, string(data))
+	r, _ := regexp.Compile("##")
+
+	test := fmt.Sprintf("%s\n", r.FindAllString(string(data), len(data)))
+	fmt.Print(len(data), test)
+	// for _, line := range data {
+	// fmt.Print(string(line))
+	// lineInChangeLog := string(line)
+	// fmt.Printf("%s\n", r.FindString(lineInChangeLog))
+	// }
+
+	file, err := os.Open("./docs/CHANGELOG.md")
+
+	if (err) != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
 }
